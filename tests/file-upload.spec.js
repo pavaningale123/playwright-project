cd625fc747a3add1128f87368c3ff14b8fee9c32
Pavan Ingale <pavaningale7849@gmail.com>
2026-04-09 11:11:50 +0530

    modification in files

--- tests/file-upload.spec.js
+++ tests/file-upload.spec.js
@@ -3,9 +3,9 @@ import{test,expect} from '@playwright/test';
 
 test('file upload',async ({page}) =>{
 
-    await page.goto("https://www.file.io/");
-    const filePath = 'C:\\Users\\Pavan Ingale\\OneDrive\\Desktop\\New folder\\11.docx';
-    await page.getByText('Upload Files').setInputFiles(filePath);
+    // await page.goto("https://www.file.io/");
+    // const filePath = 'C:\\Users\\Pavan Ingale\\OneDrive\\Desktop\\New folder\\11.docx';
+    // await page.getByText('Upload Files').setInputFiles(filePath);
         
     await page.waitForTimeout(10000);
 });
