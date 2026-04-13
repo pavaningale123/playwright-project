8173a0e6fd4a5f3bd19228c7de70ed093a5a876d
Pavan Ingale <pavaningale7849@gmail.com>
2026-04-13 17:50:48 +0530

    playwright config made changes

--- playwright.config.js
+++ playwright.config.js
@@ -15,25 +15,33 @@ import { defineConfig, devices } from '@playwright/test';
 export default defineConfig({
   testDir: './tests',
   /* Run tests in files in parallel */
-  fullyParallel: true,
+  fullyParallel: process.env.CI ? false : true,
   /* Fail the build on CI if you accidentally left test.only in the source code. */
   forbidOnly: !!process.env.CI,
   /* Retry on CI only */
   //retries: process.env.CI ? 2 : 0,
   //retries:1,
+  retries: process.env.CI ? 2 : 0,
   /* Opt out of parallel tests on CI. */
-  workers: process.env.CI ? 2 : 4,
+ workers: process.env.CI ? 2 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   //reporter: 'html',
-  reporter: [['allure-playwright', { outputFolder: 'allure-results' }]],
+  reporter: [ ['html'],['allure-playwright', { outputFolder: 'allure-results' }]],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
-  use: {
-    /* Base URL to use in actions like `await page.goto('')`. */
-    // baseURL: 'http://localhost:3000',
+  // use: {
+  //   /* Base URL to use in actions like `await page.goto('')`. */
+  //   // baseURL: 'http://localhost:3000',
 
-    /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
-    trace: 'on-first-retry',
-  },
+  //   /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
+  //   trace: 'on-first-retry',
+  // },
+
+  use: {
+  baseURL: process.env.BASE_URL || 'https://your-app-url.com',
+  trace: 'on-first-retry',
+  screenshot: 'only-on-failure',
+  video: 'retain-on-failure',
+},
 
   /* Configure projects for major browsers */
   projects: [
